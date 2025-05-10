a9c258a6dd664f03e8dfb8dddadb498207e93ee2
Yosh <ysdeveloper200@outlook.com>
2025-05-10 16:41:32 +0400

    first attempt with trade data loader

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """FundingPips Trade Detector Entery Point Module"""
 
 from core.config import load_config, setup_logging
+from trades.loader import TradeLoader
 import logging
 
 
@@ -12,3 +13,8 @@ config = load_config()
 logger.info(f"Running in Mode {config.mode}")
 logger.info(f"Output will be saved to: {config.output_dir}")
 logger.debug(f"Data source: {config.data_source.type} @ {config.data_source.path}")
+
+loader = TradeLoader(config)
+
+df = loader.load_trades()
+logger.info(f"Trade DataFrame loaded with shape: {df.shape}")
--- trades/loader.py
+++ trades/loader.py
@@ -2,3 +2,40 @@
 
 This module provides functions to load trades from various sources, including CSV files.
 """
+
+import logging
+import pandas as pd
+from pathlib import Path
+from core.config import AppConfig
+
+logger = logging.getLogger("mian")
+
+
+class TradeLoader:
+    """Service class to load trades from different data sources."""
+
+    def __init__(self, config: AppConfig):
+        self.config = config
+
+    def load_trades(self) -> pd.DataFrame:
+        """Main entrypoint: Load trades based on config source."""
+        source_type = self.config.data_source.type.lower()
+        source_path = Path(self.config.data_source.path)
+
+        if source_type == "csv":
+            return self._load_csv(source_path)
+        elif source_type == "sqlite":
+            return self._load_sqlite(source_path)
+        else:
+            raise ValueError(f"Unsupported data source type: {source_type}")
+
+    def _load_csv(self, path: Path) -> pd.DataFrame:
+        if not path.exists():
+            raise FileNotFoundError(f"CSV file not found: {path}")
+        logger.info(f"Loading trades from CSV: {path}")
+        df = pd.read_csv(path, parse_dates=["open_time", "close_time"])
+        logger.debug(f"Loaded {len(df)} trades from CSV")
+        return df
+
+    def _load_sqlite(self, path: Path) -> pd.DataFrame:
+        raise NotImplementedError("SQLite loading not yet implemented.")
