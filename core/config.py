ffc4897e06ea191cf80452608807110e11144790
Yosh <ysdeveloper200@outlook.com>
2025-05-10 16:28:56 +0400

    adds config load logic & logger

--- core/config.py
+++ core/config.py
@@ -1,1 +1,38 @@
 """Config loading and management module"""
+
+import os
+from pydantic import BaseModel, Field
+from typing import Literal
+import yaml
+import logging.config
+
+
+class DataSourceConfig(BaseModel):
+    type: Literal['csv', 'sqlite']
+    path: str
+
+
+class AppConfig(BaseModel):
+    mode: Literal['A', 'B']
+    output_dir: str = "reports/"
+    data_source: DataSourceConfig
+
+
+def load_config(path: str = "config/default.yaml") -> AppConfig:
+    with open(path, "r") as f:
+        data = yaml.safe_load(f)
+    return AppConfig(**data)
+
+
+def setup_logging(log_cfg_path="config/logger.yaml"):
+    with open(log_cfg_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # Ensure log directory exists (if file handler is used)
+    for handler in config.get("handlers", {}).values():
+        if handler.get("class") == "logging.FileHandler":
+            log_path = handler.get("filename")
+            if log_path:
+                os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+    logging.config.dictConfig(config)
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 """FundingPips Trade Detector Entery Point Module"""
+
+from core.config import load_config, setup_logging
+import logging
+
+
+setup_logging()
+logger = logging.getLogger("main")
+
+config = load_config()
+
+logger.info(f"Running in Mode {config.mode}")
+logger.info(f"Output will be saved to: {config.output_dir}")
+logger.debug(f"Data source: {config.data_source.type} @ {config.data_source.path}")
